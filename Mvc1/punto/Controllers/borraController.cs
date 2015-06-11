3ec6577bc519fd275c3796739d71f22a3b545429
shurget <ohyeahcoool@gmail.com>
2015-06-11 15:41:11 +0200

    hola

---
+++ Mvc1/punto/Controllers/borraController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace punto.Controllers
+{
+    public class borraController : Controller
+    {
+        //
+        // GET: /borra/
+
+        public ActionResult Index()
+        {
+            punto.Models.puntoencuentroEntities bd = new Models.puntoencuentroEntities();
+            punto.Models.tbevento n = new Models.tbevento();
+            //n.estado = 1;
+            bd.tbevento.Add(n);
+            if (bd.SaveChanges() == 1)
+            {
+                // correcto
+            }
+            else { 
+                //errores en informacion
+            }
+            
+            return View();
+        }
+
+    }
+}
---
+++ Mvc1/punto/Models/tbevento_m.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace punto.Models
+{
+    public partial class tbevento
+    {
+        [MetadataType(typeof(itbevento))]
+        public void prueba() {
+            this.estado = 1*5;
+        }
+    }
+    public interface itbevento{
+        [Required]
+        public object estado { get; set; }
+    }
+
+    
+}
