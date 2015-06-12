ee23c5a9d269bc17c4d7e55d504119025a17096b
shurget <ohyeahcoool@gmail.com>
2015-06-12 16:57:07 +0200

    PuntoEncuentro

---
+++ PuntoDeEncuentro/punto/punto/Controllers/PersonaController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using punto.Models;
+
+namespace punto.Controllers
+{
+    public class PersonaController : Controller
+    {
+        //
+        // GET: /Persona/
+        /// <summary>
+        /// agregando el context(el manejado de la base de datos)
+        /// </summary>
+        puntoencuentroEntities db = new puntoencuentroEntities();
+
+        public ActionResult Index()
+        {
+            this.ViewBag.imprime="hola";
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Crear() {
+            return View();
+        }
+        /// <summary>
+        /// agrega un registro a la tabla persona
+        /// </summary>
+        /// <param name="nuevo">el enviado desde el formulario del cliente</param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Crear(tbpersona nuevo) {
+            // verifica los datos introducidos
+            //para trabajar con view
+            if (ModelState.IsValid) {
+                db.tbpersona.Add(nuevo);
+                if (db.SaveChanges() > 0) {
+                    Redirect("index");
+                }
+            }
+            //mandamos el modelo para que el usuario
+            //verifique los errores encontrados
+            return View(nuevo);
+        }
+
+    }
+}
---
+++ PuntoDeEncuentro/punto/punto/Models/tbcategoria_m.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcApplication2.Models
+{
+    public partial class tbcategoria
+    {
+        [MetadataType(typeof(itcategoria))]
+        puntoencuentroEntities db = new puntoencuentroEntities();
+        public void prueba2()
+        {
+
+        }
+
+
+        public interface itcategoria
+        {
+            
+            [Key]
+            [ScaffoldColumn(false)] 
+            object idcategoria { get; set; }
+
+            
+            [MinLengthAttribute(2)]
+            object nombre { get; set; }
+
+
+            [Required]
+            object responsable { get; set; }
+
+
+            [Required]
+            object padre { get; set; }
+
+            [DataType(DataType.DateTime, ErrorMessage = "debe introducir una fecha valida")]
+            object fechacreacion { get; set; }
+
+            [DataType(DataType.DateTime, ErrorMessage = "debe introducir una fecha valida")]
+            object fechamodificacion { get; set; }
+
+            [Required]
+            object estado { get; set; }
+
+            
+
+
+        } 
+    }
+}
